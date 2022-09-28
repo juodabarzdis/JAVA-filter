89162ee9fdfed04e20d33de6694bddc527fe3ed5
Lukas Baksys <101624833+juodabarzdis@users.noreply.github.com>
2022-09-28 08:59:03 +0300

    working version.

--- ReadFile.java
+++ ReadFile.java
@@ -4,29 +4,26 @@ import java.util.Scanner;
 
 public class ReadFile {  
   public static void main(String[] args) {  
-        try {       
+      try {       
         File myObj = new File("content.txt");
-        Scanner reader = new Scanner(myObj);  
-        while (reader.hasNextLine()) {
-          String data = reader.nextLine();
-          data = data.replaceAll("[^\\d]", " ");
-          data = data.trim();
-          data = data.replaceAll(" +", " ");
-            for (String retval: data.split(" ")) {
-          int num = Integer.parseInt(retval);
-            if (num >= 10) {
-              String str = Integer.toString(num);
-              if(data.contains(str)) {
-                System.out.println(str);
-              }
-            }
-       }   
-    }
+        Scanner reader = new Scanner(myObj);
+        while (reader.hasNextLine()) {           
+        String data = reader.nextLine();    
+        String rep = data.replaceAll("[^\\d]", " ");
+        rep = rep.trim();
+        rep = rep.replaceAll(" +", " ");
+          for (String retval: rep.split(" ")) {
+        int num = Integer.parseInt(retval);
+          if (num >= 10) {
+              System.out.println(data);
+          }    
+        } 
+      }
       reader.close();
-
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
   }
 }
+
